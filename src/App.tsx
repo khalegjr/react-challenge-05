67f0fc20d049908b8945c82f57b584a8248a89de
Khaled <khalegjr@gmail.com>
2023-03-13 21:46:18 -0300

    instala axios e testa chamada a api

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import axios from "axios";
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon")
+      .then((response) => console.log(response));
+  }, []);
+
   return (
     <>
       <h1>Consumir API Pokémon</h1>
